9f11241a89c79f664cacc57bdc8c75b4d7779691
Daniel Flower <git@danielflower.com>
2026-07-12 21:56:50 +0800

    Resolve update script symlink path

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-ROOT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_PATH=$(readlink -f -- "${BASH_SOURCE[0]}")
+ROOT_DIR=$(cd -- "$(dirname -- "${SCRIPT_PATH}")" && pwd)
 
 cd -- "${ROOT_DIR}"
 git pull --ff-only
