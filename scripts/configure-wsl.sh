37a7b75b694410f978d5b923fc252287ccc79df6
Daniel Flower <git@danielflower.com>
2026-07-12 21:21:23 +0800

    Fix WSL config backup permissions

--- scripts/configure-wsl.sh
+++ scripts/configure-wsl.sh
@@ -29,7 +29,9 @@ trap 'rm -f "$tmp"' EXIT
 } >"$tmp"
 
 if [[ -e /etc/wsl.conf ]] && ! cmp -s "$tmp" /etc/wsl.conf; then
-  backup_once /etc/wsl.conf
+  if [[ ! -e /etc/wsl.conf.wsl-dev-bootstrap.bak ]]; then
+    sudo cp -a /etc/wsl.conf /etc/wsl.conf.wsl-dev-bootstrap.bak
+  fi
 fi
 
 sudo install -m 0644 "$tmp" /etc/wsl.conf
